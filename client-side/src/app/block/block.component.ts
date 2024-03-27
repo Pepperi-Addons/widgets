7fdcaf1ff8f04d79bfdc87e0c5a3c5891192dd10
Dor Shpigel <dor.s@pepperi.com>
2024-03-27 17:21:18 +0200

    initial working poc

--- client-side/src/app/block/block.component.ts
+++ client-side/src/app/block/block.component.ts
@@ -1,23 +1,25 @@
-import { TranslateService } from '@ngx-translate/core';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TranslateService } from "@ngx-translate/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-    selector: 'page-block',
-    templateUrl: './block.component.html',
-    styleUrls: ['./block.component.scss']
+  selector: "page-block",
+  templateUrl: "./block.component.html",
+  styleUrls: ["./block.component.scss"],
 })
 export class BlockComponent implements OnInit {
-    @Input() hostObject: any;
+  @Input() hostObject: any;
+  externalScript = `<script id=“ze-snippet” src=“https://static.zdassets.com/ekr/snippet.js?key=fdbd81aa-20c5-4c44-975d-0956919574b7“></script>`;
+  @Output() hostEvents: EventEmitter<any> = new EventEmitter<any>();
 
-    @Output() hostEvents: EventEmitter<any> = new EventEmitter<any>();
+  constructor(private translate: TranslateService) {}
 
-    constructor(private translate: TranslateService) {
-    }
+  ngOnInit(): void {
+    debugger;
+    const script = document.createElement("script");
+    script.id = "ze-snippet";
+    script.src = 'https://static.zdassets.com/ekr/snippet.js?key=fdbd81aa-20c5-4c44-975d-0956919574b7';
+    document.body.appendChild(script);
+  }
 
-    ngOnInit(): void {
-    }
-
-    ngOnChanges(e: any): void {
-
-    }
+  ngOnChanges(e: any): void {}
 }
